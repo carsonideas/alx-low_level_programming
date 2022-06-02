98c46591cc4add44ac23426c63d98d50ac53c3aa
carsonideas <root@DESKTOP-BR7CH99.localdomain>
2022-06-03 02:42:36 +0300

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+nclude <stdio.h>
+/**
+ *  * main - main block
+ *   * Return: 0
+ *    */
 int main(void)
-	/**
-	 *  * main - main block
-	 *   * Return: 0
-	 *    */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
