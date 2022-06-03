6ce6a6398f7e96e2237efa766b25b06bd746adcd
carsonideas <douglascarson11212gmail.com>
2022-06-03 03:05:24 +0300

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 
 /**
  *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ *    * Return: 0
  *     */
+
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
