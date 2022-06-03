9dd7666b94f131e28fdbf3fe515a0e0d04ffe264
carsonideas <douglascarson11212gmail.com>
2022-06-03 03:15:01 +0300

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,16 @@
-#nclude <stdio.h>
-
+nclude <stdio.h>
 /**
- *  *  main - Enty point
+ *  *main - entry point
  *   *
- *    *  Return: Always 0 (Success)
+ *    *Return: Always zero (success)
  *     */
-
 int main(void)
 {
-		char acharacter;
-			int ainteger;
-				long along;
-					long long alonglong;
-						float afloat;
 
-							printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-								printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
-									printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-										printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-											printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
-
-												return (0);
-
+		printf("Size of a char: %d byte(s)\n", sizeof(char));
+			printf("Size of an int: %d byte(s)\n", sizeof(int));
+				printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+					printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+						printf("Size of a float: %d byte(s)\n", sizeof(float));
+							return (0);
 }
